4c97cd2f9c183ab39864b97b71c3a8fe2f8665f6
DWWM <ben.voydie64@gmail.com>
2025-11-05 11:00:19 +0100

    push de la page A propos+ le header.vue (en cours)

---
+++ src/components/header.vue
@@ -0,0 +1,57 @@
+<template>
+  <header class="header">
+    <!-- Logo -->
+    <div class="logo-container">
+      <img src="public/logo.png" alt="Logo du site" class="logo" />
+      <h1>App Météo</h1>
+    </div>
+
+    <!-- Navigation -->
+    <nav class="nav">
+      <RouterLink to="/">Accueil</RouterLink>
+      <RouterLink to="/about">À propos</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+
+import logo from '@/public/logo.png'
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #1e1e2f;
+  color: white;
+  padding: 10px 30px;
+  box-shadow: 0 2px 5px rgba(0,0,0,0.2);
+}
+
+.logo-container {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.logo {
+  width: 50px;
+  height: 50px;
+  object-fit: contain;
+}
+
+.nav {
+  display: flex;
+  gap: 20px;
+}
+
+.nav a {
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+
+</style>
--- src/views/APropos.vue
+++ src/views/APropos.vue
@@ -5,6 +5,21 @@ import { ref } from "vue";
 
 <template>
 
+    <div class="about">
+    <h1>À propos de notre application météo</h1>
+    <p>
+      Cette application affiche la météo en temps réel à partir d’une API publique.
+      Elle a été développée avec Vue.js et vite par Méganne, Esteban, Khalil et Benjamin. 
+    </p>
+
+    <section>
+      <h2>Notre mission</h2>
+      <p>
+        Fournir des informations météorologiques précises et rapides, avec une interface simple et moderne.
+      </p>
+    </section>
+  </div>
+
 </template>
 
 
