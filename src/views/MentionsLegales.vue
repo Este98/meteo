d223d27fbd2b5ae636ae79fb011e9440a5978d77
DWWM <froudage@gmail.com>
2025-11-05 09:24:49 +0100

    feat logo et mentions légales

--- src/views/MentionsLegales.vue
+++ src/views/MentionsLegales.vue
@@ -4,7 +4,43 @@
 
 
 <template>
+    <h1>Mentions légales</h1>
+    <h2>Identité de l'éditeur :</h2>
+    <p>
+    Nom prénom <br>
+    entrepreneur individuel <br>
+    Météapp <br>
+    Adresse de domiciliation de l'EI <br>
+    Numéro SIRET <br>
+    Numéro TVA si nécessaire <br>
+    Numéro RCS/RM si nécessaire</p>
 
+    <h2>Coordonnées de l'éditeur :</h2>
+    <p>
+    Mail <br>
+    Numéro de téléphone</p>
+    
+    <h2>Identité de l'hébergeur :</h2>
+    <p>
+    Dénomination ou raison sociale <br>
+    Nom <br>
+    Adresse</p>
+
+    <h2>Coordonnées de l'hébergeur :</h2>
+    <p>
+    Mail <br>
+    Numéro de téléphone</p>
+
+    <h2>Identité du directeur de la publication :</h2>
+    <p>
+    Dénomination ou raison sociale <br>
+    Nom <br>
+    Adresse</p>
+
+    <h2>Coordonnées du directeur de la publication :</h2>
+    <p>
+    Mail <br>
+    Numéro de téléphone</p>
 </template>
 
 
